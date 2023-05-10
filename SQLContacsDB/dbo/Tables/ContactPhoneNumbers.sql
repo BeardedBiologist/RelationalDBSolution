13b7a4573d6a46d259a347c3f2ec2e342e48684c
Joshua James <contact@joshuajames.me>
2023-05-10 16:41:35 +0200

    created SQL db, locally.

---
+++ SQLContacsDB/dbo/Tables/ContactPhoneNumbers.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ContactPhoneNumbers]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ContactID] INT NOT NULL, 
+    [PhoneNumberID] INT NULL
+)
---
+++ SQLContacsDB/dbo/Tables/Contacts.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Contacts]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [firstName] NVARCHAR(50) NOT NULL, 
+    [lastName] NVARCHAR(50) NOT NULL
+)
